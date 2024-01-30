f0d063bf0a8e1cf4e3656925e958c19148220db1
sypvph <156061612+sypvph@users.noreply.github.com>
2024-01-30 20:23:43 +0700

    ontap

---
+++ ontap/projectASM/admin/javascript/create.js
@@ -0,0 +1,48 @@
+const form = document.getElementById("form");
+const titleElement = document.getElementById("title");
+const imageElement = document.getElementById("image");
+const descriptionElement = document.getElementById("description");
+const categoryElement = document.getElementById("category");
+const priceElement = document.getElementById("price");
+const API_URL = "http://localhost:3000/products"; // end-point
+
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  form.addEventListener("submit", handleSubmit);
+}
+
+async function handleSubmit(event) {
+  //1. ngan chan preventDefault()
+  event.preventDefault();
+
+  // 2. Lay du lieu input: element.value
+  const title = titleElement.value;
+  const image = imageElement.value;
+  const description = descriptionElement.value;
+  const category = categoryElement.value;
+  const price = priceElement.value;
+
+  // 3. Check validate: if(!value_1 || !value_2)
+  if (!title || !image || !category) {
+    alert("Nhap title, image, category vao");
+    return;
+  }
+
+  const newProduct = {
+    title, // newProduct.title = title
+    image,
+    description,
+    category,
+    price: price ? Number(price) : 0, // chuyen price sang number
+  };
+
+  console.log(newProduct);
+  // 4. Call API POST json-server: localhost:3000/products, newProduct
+  await fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify(newProduct),
+  });
+  // bay sang man list.html
+  window.location.replace("./list.html");
+}
